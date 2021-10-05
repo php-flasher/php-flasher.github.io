9de268bdee17cda92cdd18fd964353c426232159
KHOUBZA Younes <younes.khoubza@smile-maroc.com>
2021-10-05 02:07:37 +0100

    Wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ module.exports = {
   purge: [
       './_site/**/*.html',
   ],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'media', // or 'media' or 'class'
   theme: {
     extend: {},
   },
