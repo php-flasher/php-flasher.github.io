5b229834c7e093b44fc9437e00a6666a8fd9f238
KHOUBZA Younes <younes.khoubza@smile-maroc.com>
2022-06-13 15:02:20 +0100

    fix clipboard button not showing in the page

--- assets/js/_clipboard.js
+++ assets/js/_clipboard.js
@@ -1,24 +1,29 @@
 import "../css/_clipboard.pcss";
 
-const codeBlocks = document.querySelectorAll("pre.highlight");
+const codeBlocks = document.querySelectorAll("pre > code");
 
 codeBlocks.forEach(function (codeBlock) {
     const copyButton = document.createElement("button");
-    copyButton.className = "copy";
+    copyButton.classList.add("copy", "text-indigo-500");
     copyButton.type = "button";
     copyButton.ariaLabel = "Copy code to clipboard";
-    copyButton.innerText = "Copy";
 
-    codeBlock.append(copyButton);
+    const copyIcon = '<i class="fa-duotone fa-clipboard"></i>';
+    copyButton.innerHTML = copyIcon;
+
+    const parent = codeBlock.parentElement;
+    parent.classList.add("copyable");
+
+    parent.append(copyButton);
 
     copyButton.addEventListener("click", function () {
-        const code = codeBlock.querySelector("code").innerText.trim();
+        const code = codeBlock.innerText.trim();
         window.navigator.clipboard.writeText(code);
 
-        copyButton.innerText = "Copied";
+        copyButton.innerHTML = '<i class="fa-duotone fa-clipboard-check"></i>';
 
         setTimeout(function () {
-            copyButton.innerText = "Copy";
+            copyButton.innerHTML = copyIcon;
         }, 1000);
     });
 });
